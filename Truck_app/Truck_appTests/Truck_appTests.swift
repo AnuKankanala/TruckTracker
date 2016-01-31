c7b172769042868c10852b3d1ee6507cbb276773
Anusha Kankanala <anushakankanala@Anushas-MacBook-Air.local>
2016-01-30 19:36:21 -0800

    added fire base and updated the project

---
+++ Truck_app/Truck_appTests/Truck_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Truck_appTests.swift
+//  Truck_appTests
+//
+//  Created by Anusha Kankanala on 12/14/15.
+//  Copyright © 2015 Anusha Kankanala. All rights reserved.
+//
+
+import XCTest
+@testable import Truck_app
+
+class Truck_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
